edd34804ed0dbac2fb5fedd9ed1d91ad0be07bdb
JDRV17 <123598113+JDRV17@users.noreply.github.com>
2023-04-25 08:26:18 -0500

    Add files via upload

---
+++ math_game.py
@@ -0,0 +1,19 @@
+def add(num_1, num_2):
+  result = int(num_1) + int(num_2)
+  print(f'{num_1} + {num_2} is equal to {result}')
+  return result
+
+def substract(num_1, num_2):
+  result = int(num_1) - int(num_2)
+  print(f'{num_1} - {num_2} is equal to {result}')
+  return result
+
+def multiply(num_1, num_2):
+  result = int(num_1) * int(num_2)
+  print(f'{num_1} * {num_2} is equal to {result}')
+  return result
+
+def divide(num_1, num_2):
+  result = int(num_1) / int(num_2)
+  print(f'{num_1} / {num_2} is equal to {result}')
+  return result
